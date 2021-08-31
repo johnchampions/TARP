6bd0710ea18b09e0489933678125cbcdf8755c67
johnmatthews7234 <john@champions.tech>
2021-09-01 08:37:41 +1000

    Configure cleanout

---
+++ config.py
@@ -0,0 +1,7 @@
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:Merkin12@flaskydb.ckzo2f8bjq9z.ap-southeast-2.rds.amazonaws.com:3306/flasky'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://connecty:Merkin12@localhost:3306/flasky'
+# Uncomment the line below if you want to work with a local DB
+SQLALCHEMY_POOL_RECYCLE = 3600
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'DEV'
