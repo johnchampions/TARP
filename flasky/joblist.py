b51bb742f1d839b9cc5ab2218cd64d231adbd0d8
johnchampions <john@champions.tech>
2021-09-09 14:06:49 +1000

    Remove Turbo

--- flasky/joblist.py
+++ flasky/joblist.py
@@ -4,12 +4,13 @@ from flasky.yelpscrape2 import ys2
 import json
 from flask.templating import render_template
 from sqlalchemy.sql.expression import desc
-from flasky.models import GooglePlace, JobList, JobResults, Places, SearchCategories, YelpPlace
+from flasky.models import GooglePlace, JobList, JobResults, Places, SearchCategories, YelpPlace, ZomatoPlace
 from flask import (
     Blueprint, current_app
 )
 from flasky.db2 import db_session
 from flasky.googlescrape2 import gs2
+from flasky.zomatoscrape2 import zs2
 from flasky.tar_helper import getapikey
 import threading
 import time
@@ -51,7 +52,6 @@ def get_places(jobid):
 
 @bp.route('/jobdisplay/<int:job_id>', methods=('GET',))
 def display_job(job_id):
-    threading.Thread(target=update_restaurants, args=(job_id,)).start()
     joblist_record = JobList.query.filter(JobList.id == job_id).first()
     mydict = {
         'id': joblist_record.id,
@@ -66,14 +66,6 @@ def display_job(job_id):
     placerecords=placerecords
     return render_template('/joblist/jobdisplay.html', job=mydict, placerecords=placerecords)
 
-def update_restaurants(job_id):
-    with application.application.app_context():
-        now = time.time()
-        timeout = now + 360.0
-        while time.time() < timeout:
-            placerecords = get_restaurantlist(job_id)
-            application.turbo.push(application.turbo.replace(render_template('/joblist/restaurantlist.html', placerecords=placerecords), 'load'))
-            time.sleep(5)
 
 @bp.route('/jobrefresh/<int:job_id>', methods=('GET',))
 def refresh_job_places(job_id):
@@ -90,6 +82,7 @@ def refresh_place(id):
 def refresh_places(idlist):
     gs = gs2(getapikey('googleapikey'))
     ys = ys2(getapikey('yelpapikey'))
+    zs = zs2()
     for id in idlist:
         placerecord = Places.query.filter(Places.id == id).first()
         if placerecord.googleplaceid is not None:
@@ -98,6 +91,10 @@ def refresh_places(idlist):
         if placerecord.yelpplaceid is not None:
             ysid = YelpPlace.query.filter(YelpPlace.placeid == id).first().yelpplace_id
             ys.get_place_details((ysid,), refresh=True)
+        if placerecord.zomatoplaceid is not None:
+            zsid = ZomatoPlace.query.filter(ZomatoPlace.placeid == id).first().zomatoplace_id
+            #TODO: FIX THIS!!!!
+            #zs.get_place_details((zsid,), refresh=True)
 
 def get_restaurantlist(jobid=0):
     output = []
