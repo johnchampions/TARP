793916167cf605499262624b648cd2aeda578fd7
johnmatthews7234 <john.matthews72@gmail.com>
2022-07-15 14:21:32 +1000

    now quieter

--- .platform/hooks/postdeploy/99-install-chromedriver.sh
+++ .platform/hooks/postdeploy/99-install-chromedriver.sh
@@ -4,9 +4,9 @@ fullver=$(google-chrome --version)
 set $fullver
 chromever=$(echo "${3%%.*}")
 cd /tmp/
-sudo wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chromever
+wget -q https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chromever
 cdver=$(cat LATEST_RELEASE_$chromever)
-sudo wget https://chromedriver.storage.googleapis.com/$cdver/chromedriver_linux64.zip
-sudo unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/bin/chromedriver
+wget -q https://chromedriver.storage.googleapis.com/$cdver/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/bin/chromedriver
 chromedriver --version
