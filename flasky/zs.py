ac5cff4a4fcc796cdeee4ab8d67032a21d611f64
johnmatthews7234 <john.matthews72@gmail.com>
2022-07-19 16:20:35 +1000

    heaqdleess

--- flasky/zs.py
+++ flasky/zs.py
@@ -103,7 +103,7 @@ class zomatosearch:
         chrome_options.add_argument('--disable-extensions')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
-        #chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--window-size=1920x1080')
         chrome_options.add_argument('--user-agent="' + headers['User-agent'] + '"')
         driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
