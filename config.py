85302e0cecb25af602d10e1851f5d4e567003a01
johnmatthews7234 <john.matthews72@gmail.com>
2022-08-09 13:45:07 +1000

    Point to Prod

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
-ENVIRONMENT = ''
+ENVIRONMENT = 'PROD'
 
 if ENVIRONMENT == 'DEV':
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flasky:Merkin12@192.168.1.23:3306/flasky'
     URL = 'http://127.0.0.1/'
-else:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:Merkin12@flaskydb-instance-1.ckzo2f8bjq9z.ap-southeast-2.rds.amazonaws.com:3306/flasky'
-    URL = 'http://flasky.eba-hw3xm2pn.ap-southeast-2.elasticbeanstalk.com/'
-
 if ENVIRONMENT == 'GOGO':
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flasky:Merkin12@172.30.64.1:3306/flasky'
     URL = 'http://127.0.0.1:5000/'
+if ENVIRONMENT == 'PROD':
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:Merkin12@flaskydb-instance-1.ckzo2f8bjq9z.ap-southeast-2.rds.amazonaws.com:3306/flasky'
+    URL = 'http://flasky.eba-hw3xm2pn.ap-southeast-2.elasticbeanstalk.com/'
+
+
 
 SQLALCHEMY_POOL_RECYCLE = 3600
 SQLALCHEMY_TRACK_MODIFICATIONS = False
